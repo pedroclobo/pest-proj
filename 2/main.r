4d10514ba82e2a914107cdebed58e44f5f2c8637
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-04-27 21:50:19 +0100

    Repeated code in now in a function

--- 2/main.r
+++ 2/main.r
@@ -6,18 +6,15 @@ library(tidyr)
 anos <- read_excel("EsperancaVida.xlsx", range = "A9:A70")
 paises <- c("ES - Espanha", "PT - Portugal", "IT - Itália")
 
-homens <- read_excel("EsperancaVida.xlsx", range = "AJ9:BQ70") |>
-	select(all_of(paises)) |> mutate(anos) |> rename(Ano = 4) |>
-	subset(Ano %in% 2002:2019) |> cbind(Sexo = "Masculino") |>
-	pivot_longer(all_of(paises), names_to = "País", values_to = "Valor")
+process_data <- function(excel_range, sexo) {
+	return(read_excel("EsperancaVida.xlsx", range = excel_range) |>
+		select(all_of(paises)) |> mutate(anos) |> rename(Ano = 4) |>
+		subset(Ano %in% 2002:2019) |> cbind(Sexo = sexo) |>
+		pivot_longer(all_of(paises), names_to = "País", values_to = "Valor"))
+}
 
-mulheres <- read_excel("EsperancaVida.xlsx", range = "BR9:CY70") |>
-	select(all_of(paises)) |> mutate(anos) |> rename(Ano = 4) |>
-	subset(Ano %in% 2002:2019) |> cbind(Sexo = "Feminino") |>
-	pivot_longer(all_of(paises), names_to = "País", values_to = "Valor")
-
-homens |>
-	rbind(mulheres) |>
+process_data("AJ9:BQ70", "Masculino") |>
+	rbind(process_data("BR9:CY70", "Feminino")) |>
 	ggplot(aes(x = as.factor(Ano), y = Valor, color = País, shape = Sexo)) +
 	geom_point() +
 	labs(x = "Ano", y = "Esperança de vida à nascença (anos)")

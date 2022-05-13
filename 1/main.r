ceb137345032125b81e2375a87ea60d194a7f2af
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-05-13 12:11:09 +0100

    Cleaner code and more space on paper

--- 1/main.r
+++ 1/main.r
@@ -3,10 +3,10 @@ library(ggplot2)
 library(tidyr)
 
 dados <- read_excel("ResiduosPerCapita.xlsx", col_names = FALSE, range = "A13:C43")
-names(dados) <- c("País", "2004", "2018")
+colnames(dados) <- c("País", "2004", "2018")
 
 dados |>
 	subset(País %in% c("PT - Portugal", "NL - Países Baixos", "UK - Reino Unido")) |>
 	pivot_longer(c(`2004`, `2018`), names_to = "Ano", values_to = "Valor") |>
-	ggplot(aes(x = País, y = Valor, fill = Ano)) + geom_col(position = "dodge") +
-	labs(x = "País", y = "Produção de resíduos per capita (t)")
+	ggplot(aes(País, Valor, fill = Ano)) + geom_col(position = "dodge") +
+	labs(x = "País", y = "Produção de Resíduos Per Capita [t]")

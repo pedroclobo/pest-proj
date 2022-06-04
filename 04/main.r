c2aaffcec58ec738bbbea3d7fe91c3f9a8a626ec
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-06-04 19:55:48 +0100

    Add units to label and comments

--- 04/main.r
+++ 04/main.r
@@ -5,5 +5,5 @@ dados <- read_excel("Utentes.xlsx", range = "A1:D77")
 
 dados |>
 	ggplot(aes(Idade, Colesterol)) +
-	geom_point() + geom_smooth() +
-	labs(x = "Idade", y = "Colesterol")
+	geom_point() + geom_smooth(method = "lm") +
+	labs(x = "Idade [Anos]", y = "Colesterol")

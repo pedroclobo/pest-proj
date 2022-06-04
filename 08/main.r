651862b94b139372ea5be323d4d40f75e80d32f8
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-06-04 20:21:33 +0100

    Round value

--- 08/main.r
+++ 08/main.r
@@ -2,10 +2,11 @@ set.seed(139)
 dimensao <- 1249
 lambda <- 3.58
 confianca <- 0.91
+m <- 1250
 
 amplitudes <- c()
 
-for (i in seq(1250)) {
+for (i in seq(m)) {
 	amostra <- rexp(dimensao, lambda)
 	media <- mean(amostra)
 	b <- qnorm((1 + confianca)/2)
@@ -13,4 +14,4 @@ for (i in seq(1250)) {
 	amplitudes <- c(amplitudes, amplitude)
 }
 
-print(mean(amplitudes))
+print(round(mean(amplitudes), 6))

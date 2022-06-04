8c7c671894f768cbecfb85b301c4b9b6a4474800
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-06-04 19:22:42 +0100

    Fix indentation

--- 8/main.r
+++ 8/main.r
@@ -3,14 +3,14 @@ dimensao <- 1249
 lambda <- 3.58
 confianca <- 0.91
 
-amplitudes <- data.frame()
+amplitudes <- c()
 
 for (i in seq(1250)) {
-    amostra <- rexp(dimensao, lambda)
-    media <- mean(amostra)
-    b <- qnorm((1 + confianca)/2)
-    amplitude <- 2 * ((b * media)/sqrt(dimensao))
-    amplitudes <- rbind(amplitudes, amplitude)
+	amostra <- rexp(dimensao, lambda)
+	media <- mean(amostra)
+	b <- qnorm((1 + confianca)/2)
+	amplitude <- (2/media) * (b/sqrt(dimensao))
+	amplitudes <- c(amplitudes, amplitude)
 }
 
-print(mean(amplitudes[, 1]))
+print(mean(amplitudes))
--- 9/main.r
+++ 9/main.r
@@ -3,14 +3,14 @@ dimensao <- 1025
 lambda <- 1.3
 confianca <- 0.98
 
-amplitudes <- data.frame()
+amplitudes <- c()
 
 for (i in seq(1350)) {
-    amostra <- rexp(dimensao, lambda)
-    media <- mean(amostra)
-    b <- qnorm((1 + confianca)/2)
-    amplitude <- 2 * ((b * media)/sqrt(dimensao))
-    amplitudes <- rbind(amplitudes, amplitude)
+	amostra <- rexp(dimensao, lambda)
+	media <- mean(amostra)
+	b <- qnorm((1 + confianca)/2)
+	amplitude <- (2 / media) * (b / sqrt(dimensao))
+	amplitudes <- c(amplitudes, amplitude)
 }
 
-print(mean(amplitudes[, 1]))
+print(mean(amplitudes))

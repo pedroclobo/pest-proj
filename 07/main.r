59062111da02f844b98db17d102e596ae4a0db68
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-06-08 22:40:15 +0100

    Small tweak

--- 07/main.r
+++ 07/main.r
@@ -5,10 +5,11 @@ n <- 6
 p <- 0.18
 vesp <- n * p
 
+amostras <- replicate(1425, rbinom(dimensao, n, p))
 medias <- c()
 
 for (i in seq(1425)) {
-	medias <- c(medias, mean(rbinom(dimensao, n, p)))
+	medias <- c(medias, mean(amostras[,i]))
 }
 
 print(round(abs(mean(medias) - vesp), 6))
